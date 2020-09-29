8e1ff5464e14fa734bd3fdbda86ee4a6170370e0
Thomas Herold <thomas.herold95@gmail.com>
2020-09-29 08:02:06 -0400

    Text color adjusting based on luminance

--- src/components/Colorbox.js
+++ src/components/Colorbox.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
+import chroma from 'chroma-js';
 import "../styles/colorBox.css";
 
 class Colorbox extends Component {
@@ -20,6 +21,8 @@ class Colorbox extends Component {
     render() {
     const { background, name, moreUrl, showLink } = this.props;
     const { copied } = this.state;
+    const isDarkColor = chroma(background).luminance() <= 0.09;
+    const isLightColor = chroma(background).luminance() >= 0.55;
     
     return (
     <CopyToClipboard text={background} onCopy={this.changeCopyState}>
@@ -28,17 +31,17 @@ class Colorbox extends Component {
           <div style={{ background }} className={`copy-overlay ${copied && "show"}` } />
           <div className={`copy-msg ${copied && "show"}` }>
               <h1>Copied!</h1>
-              <p>{background}</p>
+              <p className={isLightColor && "dark-text"}>{background}</p>
           </div>
             <div className="copy-container">
                 <div className="box-content">
-                    <span>{name}</span>              
+                    <span className={isDarkColor && "light-text"}>{name}</span>              
                 </div>
-                <button className="copy-button">Copy</button>
+                <button className={`copy-button ${isLightColor && "dark-text"}`}>Copy</button>
             </div>
             {showLink && (
             <Link to={moreUrl} onClick={e => e.stopPropagation()}>
-                <span className="see-more">More</span>
+                <span className={`see-more ${isLightColor && "dark-text"}`}>More</span>
             </Link>
             )}
             
--- src/components/SingleColorPalette.js
+++ src/components/SingleColorPalette.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import NavBar from './NavBar';
 import Colorbox from './Colorbox';
 import PaletteFooter from './PaletteFooter'; 
+import { Link } from 'react-router-dom';
 
 class SingleColorPalette extends Component {
     constructor(props) {
@@ -36,15 +37,20 @@ class SingleColorPalette extends Component {
 
     render() {
         const { format } = this.state;
-        const { paletteName, emoji } = this.props.palette; 
+        const { paletteName, emoji, id } = this.props.palette; 
         const colorBoxes = this._shades.map(color => (
-            <Colorbox key={color.id} name={color.name} background={color[format]} showLink={false} />
+            <Colorbox key={color.name} name={color.name} background={color[format]} showLink={false} />
         ));
 
         return (
-            <div className="Palette">
+            <div className="SingleColorPalette Palette">
                 <NavBar handleChange={this.changeFormat} showSlider={false} />
-                <div className="Palette-colors">{colorBoxes}</div>
+                <div className="Palette-colors">
+                    {colorBoxes}
+                    <div className="go-back ColorBox">
+                        <Link to={`/palette/${id}`} className="back-button">GO BACK</Link> 
+                    </div>
+                </div>
                 <PaletteFooter paletteName={paletteName} emoji={emoji} />
             </div>
         );
